fff3fedb44e6a9e2fe01461db5d6da5ceda2bc17
Jason Seminara <jseminara@corporaterewards.com>
2015-03-24 08:52:24 -0400

    added incoming file parser

--- program 2/lib/axioms.js
+++ program 2/lib/axioms.js
@@ -34,13 +34,14 @@
 // **end**
 // At(Eggs,C,3)
 // At(Milk,A,3)
+var fs  = require("fs");
+var kbText = fs.readFileSync('kb.txt').toString().split(' ').split('\n');
+
+console.log(kbText);
+throw '';
 
 kb = [
-  ,'At(Eggs,A,0)'
-  ,'At(Truck1,A,0)'
-  ,'At(Milk,B,0)'
-  ,'At(Eggs,C,3)'
-  ,'At(Milk,A,3)'
+  
   ,'~Move(X,L1,L2,I) At(X,L2,I+1)'
   ,'~Move(Y,L1,L2,I) At(Y,L2,I+1)'
   ,'~Load(X,Y,L,I) Loaded(X,Y,I+1)'
@@ -58,8 +59,8 @@ kb = [
   ,'Move(X,L,L1,I) Move(X,L,L2,I)'
   ,'~At(X,L,I) At(X,L,I+1) YY(Load(X,Y,L,I))'
   ,'At(X,L,I-1) ~At(X,L,I) YY(Unload(X,Y,L,I))'
-  ,'~At(X,L,I) At(X,L,I+1) ARC(Move(X,L,LL,I))'
-  ,'~At(X,L,I) At(X,L,I+1) ARC(Move(X,LL,L,I))'
+  ,'~At(X,L,I) At(X,L,I+1) ARCTAIL(Move(X,L1,LL,I))'
+  ,'At(X,L,I) ~At(X,L,I+1) ARCHEAD(Move(X,LL,L2,I))'
 ];
 
 axioms = [];
@@ -74,7 +75,7 @@ axioms = [];
     //console.log(symbols);
     sentences.forEach(function(symbols){
       symbols.split(' ').forEach(function(symbol){  
-        console.log(symbol);
+        //console.log(symbol);
         if(!(atom(symbol) in hashMap)){
           //symbolArray.push(symbol)
           hashMap[atom(symbol)] = i++;
@@ -88,9 +89,9 @@ axioms = [];
 
 items = ['Milk','Eggs'];
 trucks = ['Truck1','Truck2'];
-arcs = ['A,B','B,A,C','C,A'].reduce( function(arr, arc ) {
+arcs = ['A B','B A C','C A'].reduce( function(arr, arc ) {
   var tail;
-  arc = arc.split(',');
+  arc = arc.split(' ');
   tail = arc.shift();
 
   arc.forEach( function( head ){
@@ -101,36 +102,78 @@ arcs = ['A,B','B,A,C','C,A'].reduce( function(arr, arc ) {
 
 
 
-
 // expansion for trucks
-kb.forEach( function(ax,j,arr){
-
-  replaceToken = function(collection, regex,token,k){
+kb = kb.map( function(ax,j,arr){
+    var regex = /YY\((.+)\)/;
+    var token = /Y(?=\,)/;
     var expand = ax.match(regex);
     //console.log("*******",expansion[1].replace('Y',item));
     if(expand && expand.length >= 2){
       var ex = [];
-      collection.forEach(function(t) {
+      trucks.forEach(function(t) {
         ex.push(expand[1].replace(token,(typeof k === 'number')?t[k]:t));
       });
-      console.log(ax)
-      arr[j]= ax.replace( regex, ex.join(' ') )
+      return ax.replace( regex, ex.join(' ') );
+    }else{
+      return ax;
     }
-
-  }
-  replaceToken(trucks,/YY\((.+)\)/ , /Y(?=\,)/ )
-  replaceToken(arcs, /ARC\((.+)\)/ , /LL(?=\,)/ ,1)
-  replaceToken(arcs, /ARC\((.+)\)/ , /LL(?=\,)/ ,0)
 });
-
 
 for(var i=0; i<=3; i++){
     //var arcs = [];
-    arcs.forEach( function( arc ){
+      arcs.forEach( function( arc ){
         items.forEach( function(item){
           trucks.forEach( function(truck){
-            kb.forEach( function(ax,j,arr){
-          
+              kb.forEach( function(ax){
+
+                 var regex = /ARCTAIL\((.+)\)/;
+    var token = /LL(?=\,)/;
+
+    var expand = ax.match(regex);
+    //console.log("*******",expansion[1].replace('Y',item));
+    if(expand && expand.length >= 2){
+      var ex = [];
+      // get the arcs that have the same tail
+      arcs.filter(function(t) {
+        return t[0] === arc[0];
+      }).forEach(function(t) {
+        // generate and replace with the HEAD for each frame axiom 
+        console.log("////",t[1],arc[0])
+        if(t[1] !== arc[0]){
+
+          ex.push(expand[1].replace(token,t[1]));
+        }
+      });
+      //console.log(ex,ax)
+      ax = ax.replace( regex, ex.join(' ') )
+      console.log( "*********INSERTED TAIL",ax,arc[0]);
+    }
+
+    regex = /ARCHEAD\((.+)\)/;
+    token = /LL(?=\,)/;
+
+    expand = ax.match(regex);
+    //console.log("*******",expansion[1].replace('Y',item));
+    if(expand && expand.length >= 2){
+      var ex = [];
+      // get the arcs that have the same head
+      arcs.filter(function(t) {
+        return t[1] === arc[1];
+      }).forEach(function(t) {
+        //console.log('filter',t,arc)
+        // generate and replace with the tail for each frame axiom 
+        if(t[0] !== arc[1]){
+          ex.push(expand[1].replace(token,t[0]));
+        }
+      });
+      //console.log(ex,ax)
+      ax= ax.replace( regex, ex.join(' ') )
+    console.log( "*********REPLACED HEAD",ax ,arc[1]);
+    }
+
+
+
+
                 axioms.push(
                   ax.replace(/X(?=\,)/g,item)
                   .replace(/Y(?=\,)/g,truck)
@@ -148,4 +191,19 @@ for(var i=0; i<=3; i++){
     });
 }
 parseSymbols(axioms)
+
+axioms = axioms.concat(  [
+  'At(Eggs,C,3)'
+  ,'At(Milk,A,3)'
+
+  ,'At(Eggs,A,0)'
+  ,'-At(Eggs,B,0)'
+  ,'-At(Eggs,C,0)'
+  ,'At(Truck1,A,0)'
+  ,'-At(Truck1,B,0)'
+  ,'-At(Truck1,C,0)'
+  ,'At(Milk,B,0)'
+  ,'-At(Milk,A,0)'
+  ,'-At(Milk,C,0)'])
 console.log(axioms)
+
--- program 2/lib/frontend.js
+++ program 2/lib/frontend.js
@@ -1,23 +1,102 @@
-var a = "a At(X,L,I)\nLoaded(Eggs,Truck1,1) Loaded(Eggs,Truck1,2) b c z y At(X,L,I)\nd e f g\nend qwerty a";
 (function() {
 
-	var parseSymbols = function(sentences){
-		var hashMap = {};
-		var symbolArray = [0];
-		var symbols = sentences.split(/[\n\s]/);
-		//console.log(symbols);
-		symbols.map(function(symbol){
-			console.log(symbol);
-			if(!(symbol in hashMap)){
-				symbolArray.push(symbol)
-				hashMap[symbol] = symbolArray.length-1;
-			}
-		});
-		
-		console.log(hashMap,symbolArray)
-		return [hashMap,symbolArray];
-	};	
+  function parseIncomingFile(){
 
-	return parseSymbols(a)
+    if (process.argv.length < 3) {
+      console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+      process.exit(1);
+    }
+    
+    // Read the file and print its contents.
+
+    // filesystem utils
+    var a,row
+    , fs = require("fs")
+    // read in the knowledge base
+    , kb = fs.readFileSync('kb.txt').toString().split('\n')
+    , axioms = []
+    , filename = process.argv[2]
+    , input = {}
+    , inputData = fs.readFileSync(filename).toString().split('\n');
+
+    
+    input.arcs      = []
+    input.goal      = []
+
+    input.locations = inputData.shift().trim().split(' ') // line 0
+    input.items     = inputData.shift().trim().split(' ') // line 1
+    input.trucks    = inputData.shift().trim().split(' ') // line 2
+    input.timeLimit = +inputData.shift() // line 3
+    
+    inputData.shift() //START
+    input.startState = []
+    
+    // loop through the remainder of the file; stop if we've hit the goal state
+    while (a = inputData.shift(), a != "GOAL"){
+      
+      row = a.trim().split(' ');
+
+      // these are pairs of either trucks/locations or items/locations 
+      if(input.trucks.indexOf(row[0]) !== -1 || input.items.indexOf(row[0]) !== -1){  
+        input.startState.push(row);
+      }else{
+        // don't split the arcs; we'll fix them up later
+        input.arcs.push(a.trim());
+      }
+    }
+    
+    // we've made it into the 'GOAL' section, push in the remainder of the goal states
+    while(a = inputData.shift()){
+      input.goal.push( a.trim().split(' '));
+    } 
+
+    // the arcs may come in triples or doubles. If it's a triple, split it up into in/out arcs
+    // a b c => (a,b),(a,c)
+    input.arcs = input.arcs.reduce( function(arr, arc ) {
+      var tail;
+      arc = arc.split(' ');
+      tail = arc.shift();
+
+      arc.forEach( function( head ){
+        arr.push([tail,head]);
+      });
+      return arr;
+    },[]);
+
+    return input;
+  }
+  
+
+
+
+
+    
+console.log(parseIncomingFile())
+
+throw '';
+  function atom(symbol){
+    return symbol.replace(/^(~|-)/,'');
+  };
+
+  function parseSymbols(sentences){
+    var hashMap = {};
+    var i=1;
+    // var symbols = sentences.split(/[\n\s]/);
+    //console.log(symbols);
+    sentences.forEach(function(symbols){
+      symbols.split(' ').forEach(function(symbol){  
+        //console.log(symbol);
+        if(!(atom(symbol) in hashMap)){
+          //symbolArray.push(symbol)
+          hashMap[atom(symbol)] = i++;
+        }
+      });
+    });
+    
+    //console.log(hashMap)
+    return hashMap;
+  };  
+  console.log(atom)
+ // return parseSymbols(a);
 })();
 
