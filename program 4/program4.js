700340c23366e87fdf2e0a25b3d301802472d1df
Jason Seminara <jseminara@corporaterewards.com>
2015-04-29 23:38:03 -0400

    converted stopwords to O(1) struct lookup

--- program 4/program4.js
+++ program 4/program4.js
@@ -1,34 +1,39 @@
 (function learn(bios,N,stopwords){
 	var bios = [];
-	var stopwords = [];
-	var trainingStats = {};
+	var stopwords = {};
+	var trainingStats = {rawcount:{},stats:{}};
 
 	var fs = require('fs');
 	var events = new (require('events')).EventEmitter;
 	var files = ['stopwords.txt','bioCorpus.txt'];
 	var fileCount = 2;
 
-	String.prototype.normalize = function() {
-		return this
-			.trim()										// there might be some trailing whitespace, let's get rid of it
-	  	.toLowerCase();						// all text should be normalized to lowercase
-	};
 
 	function readBio(data) {
 		// split into records 
-	  bios = data.toString().trim()
-	  							/* The format of this file is as follows: lowercase */
-	  	.split(/\s*\n{2,}\s*/) 								/* Separate biographies are separated by 1 or more blank lines. */
+	  bios = data.toString()
+	    .trim() 										/* trim off any whitespace that could potentially become a null record*/
+	  	.split(/\s*\n{2,}\s*/) 			/* Separate biographies are separated by 1 or more blank lines. */
 	  	
 	  	//console.log(bios)
 	}
 
+	/**
+	* Parse the stopwords data file
+	*/ 
 	function readStopWords(data){
-		stopwords = data.toString().trim().split(/[\n\s]+/);
-		//console.log(stopwords);
+		// convert it into a O(n) lookup hash
+		stopwords = data.toString()     /* just to make sure it's a real string */
+			.trim()												/* trim off any whitespace that could potentially become a null record*/
+			.split(/[\n\s]+/)							/* split on any NL or spaces */
+			.reduce(function(a,b){
+				a[b] = true;
+				return a;
+			},{});
+		return;
 	}
 
-	var readFileContents = function (err,data,callback){
+	function readFileContents (err,data,callback){
 		if(err) throw err;
 		callback(data);
 		if(--fileCount===0){
@@ -38,24 +43,13 @@
 	}
 
 
-
-	function removeStopWords (allwords,badword) {
-			// dynamically generate a regexp from the stopword
-			var rgx = new RegExp('\\b\('+badword+')\\b','g');
-			//console.log(rgx);
-			return allwords.replace(rgx,'');
-	}
-
-	function tabulateWords(cat,cleanword) {
-		if(typeof trainingStats[cat] === 'undefined'){
-			trainingStats[cat] = {};
+	function tabulateWords(cat,cleanword,counter) {
+		if(typeof counter[cleanword] === 'undefined'){
+			counter[cleanword] = {};
 		}
-		if(typeof trainingStats[cat][cleanword] === 'undefined'){
-			trainingStats[cat][cleanword] = 0;
+		if(typeof counter[cleanword][cat] === 'undefined'){
+			counter[cleanword][cat] = 1;
 		}
-
-		// count here
-		trainingStats[cat][cleanword]++;
 
 	}
 
@@ -64,11 +58,9 @@
 		var wordsRemoved =[];
 		// walk through the stopwords and remove any stopwords
 
-		var trainingSet = stopwords.reduce( removeStopWords , bios.slice(0,N).join('***').toLowerCase())
-		  .split('***')
+		var trainingSet = bios.slice(0,N)
 			.map( function(el){
-
-		  	var temp = el.split(/\n/);		/* split each record into lines, */  
+		  	var temp = el.toLowerCase().split(/\n/);		/* split each record into lines, */  
 		  	var name = temp.shift().trim();  /* In each biography, the first line is the name of the person.*/
 		  	var cat  = temp.shift().trim();  /* The second line is the category (a single word). */
 		  	var desc = temp.join(' ')       /* The remaining lines are the biography. */
@@ -76,9 +68,12 @@
 		  		.reduce( function(a,el){      /* remove small words and junk from the words */
 		  			var cleanword = el.replace(/[^a-z]/g,'');
 
-		  			// if the words are longer than 2, start counting them
-						if(cleanword.length > 2){
-							tabulateWords(cat,cleanword);
+		  			// if the words are longer than 2
+		  			// AND not in the startwords lookup, 
+		  			// start counting them
+						if(cleanword.length > 2 && !stopwords[cleanword]){
+							
+							tabulateWords(cat,cleanword,trainingStats.rawcount);
 							//push this into the set
 							a.push(cleanword);
 						}
@@ -86,7 +81,6 @@
 
 		  		},[])
 		  	
-		  		console.log(desc,trainingStats);
 
 		   	return {
 		   		name	: name,				
@@ -94,6 +88,7 @@
 		   		desc  : desc			
 		   	};
 		  })
+
 
 		  //console.log(trainingSet)
 			//throw '';
@@ -104,6 +99,7 @@
 		// bios
 
 
+		console.log(trainingStats);
 		console.log('fileProcessingFinished');
 	});
 
